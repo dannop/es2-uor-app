3486423eeeb84c026126c0d7ec09e6917619b291
Ary de Souza Lopes <ary.s.lopes22@gmail.com>
2021-09-17 20:31:37 -0300

    estilizando login

--- src/pages/auth/sign-in/index.tsx
+++ src/pages/auth/sign-in/index.tsx
@@ -5,7 +5,10 @@ import LoaderContext from '../../../context/loader';
 import DefaultBtn from '../../../components/buttons/default-btn';
 import TextField from '../../../components/forms/text-field';
 import { StorageService } from '../../../services';
-import { AuthApi } from '../../../services/api-routes';
+
+import './style.scss';
+
+import logoImg from '../../../assets/images/logo.png';
 
 export default function SignInPage(props: any){
   const loaderContext = useContext(LoaderContext);
@@ -19,7 +22,7 @@ export default function SignInPage(props: any){
 
   async function login() {
     loaderContext?.setLoading(true);
-    const body = { email, password };
+    // const body = { email, password };
     const resp = {
       "type": "bearer",
       "token": "NQ.US2ogNc8BIbmlPdMSx8JX8cmer6lCQMDK3Y3apR7GFBOsUHL0r28enNJygAy"
@@ -34,33 +37,38 @@ export default function SignInPage(props: any){
   }
 
   return (
-    <div className="container">
-      <DefaultBtn 
-        label="Voltar"
-        onClick={() => props.history.push({pathname: '/'})}
-      />
-      <TextField 
-        label={'E-mail'}
-        name={'E-mail'}
-        value={email}
-        onChange={(e: any) => setEmail(e.target.value)}
-        placeholder={'Digite seu e-mail'}
-      />
-      <TextField 
-        label={'Senha'}
-        name={'Senha'}
-        value={password}
-        onChange={(e: any) => setPassword(e.target.value)}
-        placeholder={'Digite sua senha'}
-      />
-      <DefaultBtn 
-        label="Entrar"
-        onClick={() => login()}
-      />
-      <DefaultBtn 
-        label="Cadastre-se"
-        onClick={() => props.history.push({pathname: '/sign-up'})}
-      />
+    <div className="default-section">
+      <div className="container">
+        <div className="login-container">
+          <div className="col-md-6">
+            <img src={logoImg} alt="Logo do Game" />
+            <TextField 
+              label={'E-mail'}
+              name={'E-mail'}
+              value={email}
+              onChange={(e: any) => setEmail(e.target.value)}
+              placeholder={'Digite seu e-mail'}
+            />
+            <TextField 
+              label={'Senha'}
+              name={'Senha'}
+              value={password}
+              onChange={(e: any) => setPassword(e.target.value)}
+              placeholder={'Digite sua senha'}
+            />
+            <div className="">
+              <DefaultBtn 
+                label="Entrar"
+                onClick={() => login()}
+              />
+              <DefaultBtn 
+                label="Cadastre-se"
+                onClick={() => props.history.push({pathname: '/sign-up'})}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
