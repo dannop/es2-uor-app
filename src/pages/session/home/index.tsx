dd94a3de06df6493be3c1b886f9bf0b549748d44
Ary de Souza Lopes <ary.s.lopes22@gmail.com>
2021-09-17 21:39:57 -0300

    modal de home pronto

--- src/pages/session/home/index.tsx
+++ src/pages/session/home/index.tsx
@@ -19,13 +19,21 @@ export default function HomePage(props: any){
     loaderContext?.setLoading(false);
   }, [loaderContext]);
 
+  const modal_content = (
+    <div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque semper nibh eros, sed luctus mi blandit eu. Phasellus sed dictum turpis, vitae dignissim tellus. Aliquam eu nulla facilisis, volutpat arcu nec, molestie augue. Cras maximus, quam finibus laoreet ullamcorper, leo nunc sagittis diam, non sollicitudin quam libero ut tortor. </p>
+      <p>Maecenas hendrerit porttitor quam, sit amet facilisis ex suscipit eu. Morbi urna metus, ornare eget feugiat dictum, convallis sed leo. In sit amet purus turpis. Ut risus ligula, porttitor sit amet orci at, lacinia luctus magna. Pellentesque condimentum aliquet augue, sit amet tincidunt dolor condimentum non. Curabitur varius iaculis eros, eget feugiat tellus sagittis vel.</p>
+    </div>
+  )
+
   return (
     <section id="main-menu" className="d-flex">
       <DefaultModal 
         visible={rules_modal}
         setVisible={setRulesModal}
         title={"Regras"}
-        content={"Lorem ipsum"}
+        content={modal_content}
+        title_icon={<i className="fas fa-gavel align-self-center"></i>}
       />
       <div className="container-fluid">
         <div className="action-container">
