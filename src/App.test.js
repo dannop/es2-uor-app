9435597acfe5297de81fb948e6a3bfaabd596478
Daniel Lessa <daniellsf@id.uff.br>
2021-09-17 23:04:17 -0300

    Criando testes pro fluxo nao autenticado

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Cadastre-se/i);
-  expect(linkElement).toBeInTheDocument();
-});
